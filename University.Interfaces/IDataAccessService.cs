b43158772d66257c9c58873f9d2bb1f831083b10
Marcin Szewczyk <73945576+mlodyluzak@users.noreply.github.com>
2023-09-01 19:43:05 +0200

    Add files via upload

---
+++ University.Interfaces/IDataAccessService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+using University.Interfaces;
+
+namespace University.Services
+{
+    public interface IDataAccessService
+    {
+        void SaveDataToFile(string filePath);
+        void ReadDataFromFile(string filePath);
+        void AddEntity(IEntity entity);
+        void RemoveEntity(IEntity entity);
+        void EditEntity(IEntity entity);
+        ObservableCollection<T> GetEntities<T>() where T : IEntity;
+        T FindEntity<T>(string key) where T : IEntity;
+        void EnsureDeleted();
+        void SetDatabaseContext(DbContext context);
+    }
+}
---
+++ University.Interfaces/IValidationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Interfaces
+{
+    public interface IValidationService
+    {
+        bool Validate<T>(Func<T, bool> validator, T value);
+        bool ValidatePESEL(string pesel);
+        bool ValidateBirthDate(DateTime? date);
+    }
+}
